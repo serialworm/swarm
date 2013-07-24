77482f5170034afa57342ccb21b2428935015aec
Eric Beringer <eric.beringer@thinksolid.com>
2013-07-23 20:04:24 -0500

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swarm::Application.config.secret_token = '30ad897f164a4e78c721f7c7d7c715da84131c1adf13cc0290b03989179d666f17efb4ab42372ee10ff0c1c004ef0ba1092f6c4515ceb9dfc08f12d5729ce7c7'
